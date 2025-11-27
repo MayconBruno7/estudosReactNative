cb49134974986b58e5182b1613666d01ea151e2e
Maycon Bruno <114522993+MayconBruno7@users.noreply.github.com>
2025-11-26 21:59:41 -0300

    Rodando app pela primeira vez e importando um componente

---
+++ ReactNativeAprendendoRapido/aula1/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, backgroundColor: 'blue', justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 30, color: 'white' }}>TESTE</Text>
+    </View>
+  );
+}
