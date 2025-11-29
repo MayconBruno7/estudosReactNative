c1d54f35a063bfb22bdabe39c77fa4894568388e
Maycon Bruno <114522993+MayconBruno7@users.noreply.github.com>
2025-11-29 11:28:11 -0300

    PropTypes, DefaultProps, Estado e imutabilidade

--- ReactNativeAprendendoRapido/aula1/App.js
+++ ReactNativeAprendendoRapido/aula1/App.js
@@ -1,19 +1,49 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity, TextInput } from 'react-native';
 import Button from './src/components/Button';
-
 // npx react-native start
 // npx react-native run-android
 
-const App = () => {
-  return (
-    <View>
-      <Button alerta="Apertou">
-        <Text>Clicar</Text>
-      </Button>
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    contador: 0,
+    name: 'Maycon',
+  };
+
+  incrementar = () => {
+    this.setState({
+      contador: this.state.contador + 1,
+    });
+  }
+
+  alteraNome = (nome) => {
+    this.setState({
+      name: nome,
+    });
+  }
+  render() {
+    return (
+      <View>
+        <Text>Contador: {this.state.contador}</Text>
+        <TouchableOpacity onPress={this.incrementar}>
+          <Text>Incrementar</Text>
+        </TouchableOpacity>
+        <Button alerta="Apertou">
+          <Text>Clicar</Text>
+        </Button>
+        <Button>
+          <Text>Button 2</Text>
+        </Button>
+
+        <Text>Olá {this.state.name}</Text> 
+         <TextInput 
+            value={this.state.name} 
+            onChangeText={nome => this.alteraNome(nome)}
+          />
+      </View>
+    );
+  }
 }
 
 
-export default App;
+// export default App;
--- ReactNativeAprendendoRapido/aula1/src/components/Button/index.js
+++ ReactNativeAprendendoRapido/aula1/src/components/Button/index.js
@@ -1,7 +1,16 @@
+import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import { View, Text, TouchableOpacity, Alert } from 'react-native';
 
 export default class Button extends Component {
+
+    static  propTypes = {
+        alerta: PropTypes.string,
+    }
+
+    static defaultProps= {
+        alerta: 'Default Props',
+    }
     render () {
         return  (
             <TouchableOpacity onPress={() => {
