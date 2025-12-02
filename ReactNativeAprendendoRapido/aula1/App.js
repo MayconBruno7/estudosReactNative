615bf1ed2664de3f4f652de3432b907a6885e34d
Maycon Bruno <114522993+MayconBruno7@users.noreply.github.com>
2025-12-01 21:47:29 -0300

    Inicio navegação

--- ReactNativeAprendendoRapido/aula1/App.js
+++ ReactNativeAprendendoRapido/aula1/App.js
@@ -8,6 +8,7 @@ export default class App extends Component {
   state = {
     contador: 0,
     name: 'Maycon',
+    personagens: []
   };
 
   incrementar = () => {
@@ -21,6 +22,23 @@ export default class App extends Component {
       name: nome,
     });
   }
+
+  async componentWillMount(){
+    try {
+      const response = await fetch("https://swapi.dev/api/people/?format=json");
+      const dataJson = await response.json();
+
+      console.log(dataJson.results);
+      this.setState({personagens: dataJson.results});
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  // componentDidMount() {
+  //   console.log('componentDidMount')
+  // }
+  
   render() {
     return (
       <View>
@@ -36,10 +54,17 @@ export default class App extends Component {
         </Button>
 
         <Text>Olá {this.state.name}</Text> 
-         <TextInput 
-            value={this.state.name} 
-            onChangeText={nome => this.alteraNome(nome)}
-          />
+        <TextInput 
+          value={this.state.name} 
+          onChangeText={nome => this.alteraNome(nome)}
+        />
+
+        {this.state.personagens.map(personagem => {
+          console.log(personagem)
+          return <Text key={personagem.name}>{personagem.name}</Text>
+        })}
+
+
       </View>
     );
   }
--- ReactNativeAprendendoRapido/aula1/src/components/Button/index.js
+++ ReactNativeAprendendoRapido/aula1/src/components/Button/index.js
@@ -2,27 +2,17 @@ import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import { View, Text, TouchableOpacity, Alert } from 'react-native';
 
-export default class Button extends Component {
-
-    static  propTypes = {
-        alerta: PropTypes.string,
-    }
-
-    static defaultProps= {
-        alerta: 'Default Props',
-    }
-    render () {
-        return  (
-            <TouchableOpacity onPress={() => {
+const Button = () => {
+    return  (
+        <TouchableOpacity onPress={() => {
                 Alert.alert(this.props.alerta);
-            }}>
+            }}
+        >
             <View> 
-                <Text>{this.props.title}</Text>
-                {this.props.children}
+                {/* {this.props.children} */}
             </View>
-            </TouchableOpacity>
-        );
-    }
+        </TouchableOpacity>
+    );
 };
 
-// export default Button;
+export default Button;
