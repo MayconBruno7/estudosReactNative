a14ea316755dcc10091caa21346600723e7a02d6
Maycon Bruno <mayconbruno118@gmail.com>
2026-01-06 13:51:09 -0300

    Controle de estado com useState Hook, Adicionando Tarefas ao Kanban, Editando Tarefas e Eventos,  Salvando e filtrando tarefas

--- ReactParaIniciantes/meu-projeto/src/App.js
+++ ReactParaIniciantes/meu-projeto/src/App.js
@@ -3,16 +3,43 @@ import './App.css';
 
 import Navbar from './components/NavBar/Navbar';
 import TaskList from './components/NavBar/TaskList/TaskList';
+import { useState } from 'react';
+
+const task = {
+  id: 0,
+  title: 'Nova tarefa',
+  state: 'pendente'
+}
 
 function App() {
+
+  const [tasks, setTasks] = useState([])
+
+  let idAcc = 0;
+
+  const generateId = () => {
+    idAcc = idAcc + 1;
+    return idAcc;
+  }
+
+  const addTask = (title, state) => {
+    const newTask = {
+      id: generateId(),
+      title,
+      state,
+    }
+    setTasks((existingTasks) => {
+      return [
+        ...existingTasks, newTask
+      ]
+    }) 
+  }
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
-        <div>
-          <TaskList title="Pendente" />
-          <TaskList title="Fazendo" />
-          <TaskList title="Completo" />
+        <div className='container'>
+          <TaskList title="Pendente" onAddTask={addTask} tasks={tasks} />
         </div>
       </header>
     </div>
